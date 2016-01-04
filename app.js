b81032e02e95c9f3443204e1582ad110bcd070ef
ipc103 <iancandysemail@gmail.com>
2016-01-04 13:24:28 -0500

    add factory

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 angular.module('flatironGrouper', [])
+.factory('groups', [function(){
+  var o = {
+    groups: [
+      {name: "Pittsburgh Steelers", rank: 6},
+      {name: "Pittsburgh Pirates", rank: 4},
+      {name: "Pittsburgh Penguins", rank: 17, status: "Not Very Good"}
+    ]
+  };
+  return o;
+}])
 .controller('MainCtrl', [
 '$scope',
-function($scope){
-  $scope.test = 'The system is working';
-  $scope.groups = [{name: 'Steelers', status: 'Playoff Team', rank: 6}, {name: 'Pirates', status: 'Playoff Team', rank: 4}, {name: 'Penguins', status: 'Not so Good', rank: 17}];
-}]);
+'groups',
+function($scope, groups){
+    $scope.groups = groups.groups
+    $scope.addGroup = function(){
+      if ($scope.name == ''){
+        return;
+      }
+      $scope.groups.push({name: $scope.name, rank: parseInt($scope.rank), status: $scope.status})
+      $scope.name = '';
+      $scope.rank = '';
+      $scope.status = '';
+    };
+    $scope.incrementRank = function(group){
+      group.rank -= 1;
+    };
+
+    $scope.decrementRank = function(group){
+      group.rank += 1;
+    };
+  }]);
